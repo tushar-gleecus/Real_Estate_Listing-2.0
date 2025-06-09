90084289879c691ad0e8e06ee2d00b3b0e42bc23
tushar-gleecus <tushar.canchi@gleecus.com>
2025-06-09 16:34:51 +0530

    Add Render domain to ALLOWED_HOSTS

--- real_estate_listing/settings.py
+++ real_estate_listing/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&wjh@5%v6$*skl-gk2o+g1t2@&&xdf6t%fvomtiuh)@6!nn-r#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-...')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'real-estate-listing-2-0.onrender.com',   # Your Render URL (copy it from the error/log)
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
